c80535c6be24c07b3e3315b07b70c25f529ce628
Sophie Emmrich <sophie.emmrich@de.abb.com>
2022-02-16 15:44:14 +0100

    added local exec

--- app/app.py
+++ app/app.py
@@ -9,3 +9,7 @@ def hello_world():
     src = get_comic()
     return '<H1>Hello Watttron!</H1> ' \
            '<img src="%s" alt="No image could be loaded :/">' % src
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
